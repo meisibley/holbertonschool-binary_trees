a6385e86700ae1072c8cc942da64a94db549f68d
meisibley <mkong99@yahoo.com>
2023-08-18 08:42:54 -0700

    13. binary tree nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - A function that counts the nodes in a binary tree
+ * @tree: A pointer to the root node of the tree
+ *
+ * Return: count of nodes, NULL if failure
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t left_nc, right_nc, count;
+
+	if (tree == NULL)
+		return (0);
+	else if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_nc = binary_tree_nodes(tree->left);
+	right_nc = binary_tree_nodes(tree->right);
+	count = left_nc + right_nc + 1;
+	return (count);
+}
