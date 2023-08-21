ae42978ca654d33c0b24a66b5f0aed9888cc510f
meisibley <mkong99@yahoo.com>
2023-08-21 09:28:50 -0700

    100. ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  *
  * Return: A pointer to the lowest common ancestor node
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
 	const binary_tree_t *f_node = first, *s_node = second;
 
